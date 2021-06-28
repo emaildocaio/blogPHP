2353543ce16737f2b11da0e4ec585b348fe3fae5
Caio Farias <caiofarias@birah-macbook.local>
2021-06-28 10:53:09 -0300

    Convert HTML e CSS

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout >
-
+{{-- 
     <x-slot name='content'>
         @foreach ($posts as $post)
             <article class="{{ $loop -> even ? "test-class" : ""}}">
@@ -17,6 +17,24 @@
                     </div> 
             </article>
         @endforeach
-    </x-slot>
+    </x-slot> --}}
     
+    @include('_post-header')
+
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+
 </x-layout>
