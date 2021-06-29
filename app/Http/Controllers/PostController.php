98c2a120fb847b4a85f8b7b0bd8281374a7f6f5b
Caio Farias <caio.farias@zarpo.com.br>
2021-06-29 15:19:44 -0300

    Step39 - Doesnt Work

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,23 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 
-use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
-
-    public function index(){
-
+    public function index()
+    {
         return view('posts', [
-            'posts'=> Post::latest()->filter(request(['search']))->get(),
+            'posts' => Post::latest()->filter(request(['search']))->get(),
             'categories' => Category::all()
         ]);
     }
-    //Ache um post pelo id e pass para a view chamada post
-    public function show(Post $post){
+
+    public function show(Post $post)
+    {
         return view('post', [
             'post' => $post
         ]);
     }
 }
-    
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Clockwork\Storage\Search;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,24 +18,10 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('posts/{post:slug}', [PostController::class, 'show']); 
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-// Route::get('posts/{post}', function(Post $post){
-//     //Ache um post pelo id e pass para a view chamada post 
-//     return view('post', [
-//         'post' => $post
-//     ]);
-//  }); 
 
-Route::get('categories/{category:slug}', function (Category  $category){
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
-
-Route::get('authors/{author:username}', function (User $author){
+Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
         'categories' => Category::all()
