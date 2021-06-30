bd7247fd508e5231260d7d97844a1f3a1eeacbb6
Caio Farias <caio.farias@zarpo.com.br>
2021-06-30 15:45:48 -0300

    Step49 - Login logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
@@ -19,7 +20,10 @@ class RegisterController extends Controller
             'email' => ['required', 'max:255', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:7', 'max:255']
         ]);
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        //login
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsControllers;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -21,10 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [ RegisterController::class, 'create']);
-Route::post('register', [ RegisterController::class, 'store']);
+Route::get('register', [ RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [ RegisterController::class, 'store'])->middleware('guest');
 
-
+Route::post('logout', [SessionsControllers::class, 'destroy']);
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts.index', [
