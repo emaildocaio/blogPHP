0641fd133056838b9b744895a12a3455b5bae9d7
Caio Farias <caio.farias@zarpo.com.br>
2021-07-02 17:38:02 -0300

    Step54 - Make Comments Dinamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,18 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60px" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{$comment->id}}" alt="" width="60px" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Ariel Costa</h3>
+            <h3 class="font-bold">{{$comment->author->username}}</h3>
             <p class="font-xs">Posted
-                <time>8 months ago</time>
+                <time>{{$comment->created_at}}</time>
             </p>
         </header>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, aspernatur architecto? Sequi quidem suscipit iusto quaerat, dignissimos in quam quae! Quo aliquid quae, magni accusantium tempora rem alias autem. Suscipit veritatis ratione.
+            {{$comment->body}}
         </p>
     </div>
 </article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,12 +6,14 @@
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
 
                     <p class="mt-4 block text-gray-400 text-xs">
+                        Published <time>This is Harde Coded</time>
                         Published <time>{{$post->created_at->diffForHumans()}}</time>
                     </p>
 
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
+                            <h5 class="font-bold">Caio Farias</h5>
                             <h5 class="font-bold">{{$post->author->name}}</h5>
                         </div>
                     </div>
@@ -48,10 +50,13 @@
                        
                 </div>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-post-comment/>
-                    <x-post-comment/>
 
-                    <x-post-comment/>
+                    @foreach ($post->comment as $comment)
+                        <x-post-comment :comment="$comment" />
+                    @endforeach
+                        
+                    
+        
 
                 </section>
             </article>
